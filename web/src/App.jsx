9ce02e966c6d84093b11a8c066ead1a533b49740
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2024-01-10 17:20:18 -0600

    04 - Add FE dependencies, add basic todo list, form, and add button - mw

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState, useEffect } from 'react';
+// import axios from 'axios';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+// import IconButton from '@mui/material/IconButton';
+// import Typography from '@mui/material/Typography';
+import TodoList from './components/TodoList'; 
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const todos = ['Do laundry', 'Call dentist', 'Take down Christmas decorations'];
+    // const [todos, setTodos] = useState([]);
+    // const [fetchTodosError, setFetchTodosError] = useState(null);
+    // const [saveTodoError, setSaveTodoError] = useState(null);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //     //   try {
+    //         const { data } = await axios.get(`${process.env.BASE_API}/api/Todos/All`);
+    //         setTodos(data);
+    //     //   } catch (err) {
+    //     //     setFetchRecipesError(err);
+    //     //   }
+    //     //   setLoadingState(false);
+    //     };
+    //     fetchData();
+    //   }, []);
+
+    return (
+        <Container maxWidth="xl">
+            <h1>TODO</h1>
+            <Box
+                component="form"
+                sx={{
+                    '& > :not(style)': { m: 1, width: '25ch' },
+                }}
+                noValidate
+                autoComplete="off"
+            >
+                <TextField required id="standard" label="Task" variant="standard" />
+                <Button variant="outlined" startIcon={<AddIcon />} color="primary" aria-label="add todo">
+                Add
+                </Button>
+            </Box>
+            <TodoList allTodos={todos} />
+
+        </Container>
+    )
 }
 
-export default App
+export default App;
