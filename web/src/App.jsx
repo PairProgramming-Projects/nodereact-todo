63851151f930d0933f065e177ce27baf7caae0d2
Maricar Walters <64795791+mlwalters@users.noreply.github.com>
2024-01-23 12:17:37 -0600

    Add a custom theme - mw

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,6 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Container from '@mui/material/Container';
 import './App.css';
 import AppBar from './components/AppBar';
 import TodoList from './components/TodoList';
@@ -9,17 +10,33 @@ const App = () => {
         palette: {
             mode: 'light',
             primary: {
-                main: '#583979',
-                secondary: '#657d9b'
+                main: '#04475a', //darkblue
+                light: '#fcfcfd', // white
+                dark: '#00131f', // black
+                contrastText: '#d8d7e1' //gray
+            },
+            secondary: {
+                main: '#f16d26', // orange
+                light: '#c0db63', //green
+                contrastText: '#fcfcfd' //white
             }
         },
+        typography: {
+            "fontFamily": `"Roboto", "Helvetica", "Arial", sans-serif`,
+            "fontSize": 14,
+            "fontWeightLight": 300,
+            "fontWeightRegular": 400,
+            "fontWeightMedium": 500
+           }
         });
 
     return (
         <ThemeProvider theme={lightTheme}>
         <CssBaseline />
             <AppBar />
-            <TodoList />
+            <Container sx={{ height: '100vh' }} >
+                <TodoList />
+            </Container>
         </ThemeProvider>
     )
 }
